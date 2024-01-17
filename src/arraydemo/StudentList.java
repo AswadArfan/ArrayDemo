4bbd69267b2e72c5bc0432667cf17578dc0fb9dd
Aswad Arfan <arfana@sheridancollege.ca>
2024-01-17 14:07:32 -0500

    first commit

---
+++ src/arraydemo/StudentList.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemo;
+
+/**
+ *
+ * @author aswad
+ */
+public class StudentList {
+    public static void main(String[] args) {
+        Student s1 = new  Student(1, "Aswad");
+        Student[] StudentList = new Student[3];
+        StudentList[0]=s1;
+        StudentList[1]=new Student(2, "Arfan");
+        StudentList[2]=new Student(3, "ABC");
+        
+        for (int i = 0; i<StudentList.length; i++){
+            System.out.println(StudentList[i].getsName());
+        }
+    }
+}
